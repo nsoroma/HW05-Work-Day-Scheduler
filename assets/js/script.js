ae1e1c88f43e5ab9b63249bb94a9caa6cc6a8a73
Raquel <33493284+nsoroma@users.noreply.github.com>
2021-12-07 01:40:29 -0500

    rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Define currentDay - moment.js
 var currentDay = moment().format("dddd, MMMM Do");
+
 // Display currentDay
 $("#currentDay").text(currentDay);
 
@@ -15,3 +16,22 @@ var block4pm = moment().hour(16);
 var block5pm = moment().hour(17);
 
 var timeblocks = [block9am, block10am, block11am, block12pm, block1pm, block2pm, block3pm, block4pm, block5pm];
+
+var block9am = JSON.parse(localStorage.getItem('block9am')) || "";
+var block10am = JSON.parse(localStorage.getItem('block10am')) || "";
+var block11am = JSON.parse(localStorage.getItem('block11am')) || "";
+var block12pm = JSON.parse(localStorage.getItem('block12pm')) || "";
+var block1pm = JSON.parse(localStorage.getItem('block1pm')) || "";
+var block2pm = JSON.parse(localStorage.getItem('block2pm')) || "";
+var block3pm = JSON.parse(localStorage.getItem('block3pm')) || "";
+var block4pm = JSON.parse(localStorage.getItem('block4pm')) || "";
+var block5pm = JSON.parse(localStorage.getItem('block5pm')) || "";
+
+$.each(timeblocks, function (index, value) {
+  events = [block9am, block10am, block11am, block12pm, block1pm, block2pm, block3pm, block4pm, block5pm]
+  $(".container").append("<div class='row'><div class='col-2 hour text-right' id='hour" +
+      (index + 9) + "'><span>" + value.format("h A") + "</span></div><div class='col-8 event-group' id='timeblock" +
+      (index + 9) + "'><textarea class='events col-12' id='eventblock" + (index + 9) + "'>" + events[index] + "</textarea></div>" +
+      "<i class='fas fa-save' title='Save Event'></i> </i></div></div></div>");
+});
+
